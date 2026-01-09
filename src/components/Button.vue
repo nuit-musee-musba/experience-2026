6078a4667a6772ec4e3ef9a3b6f8ae2f2834ffb2
Simon Daguet-Kargl <144481209+simdgtk@users.noreply.github.com>
2026-01-09 16:27:31 +0100

    feat(Buttons): buttons done

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,98 +1,171 @@
 <template>
-  <button
-    class="btn"
-    :class="[
-      `btn--${variant}`,
-      { 'btn--icon-only': iconOnly }
-    ]"
-    :disabled="disabled"
-  >
-    <span v-if="iconLeft" class="btn__icon btn__icon--left">
-      ←
+  <button class="btn" :class="[
+    `btn--${color}`,
+    { 'btn--icon-only': iconOnly }
+  ]" :disabled="state === 'disable'">
+    <span v-if="iconPrimary" class="btn__icon btn__icon--primary">
+      <slot name="icon-primary">
+      </slot>
     </span>
 
-    <span v-if="!iconOnly" class="btn__label">
-      <slot />
+    <span v-if="!iconOnly && textContent" class="btn__label">
+      {{ textContent }}
     </span>
 
-    <span v-if="iconRight" class="btn__icon btn__icon--right">
-      →
+    <span v-if="iconSecondary" class="btn__icon btn__icon--secondary">
+      <slot name="icon-secondary">
+      </slot>
     </span>
   </button>
 </template>
 
 <script setup>
 defineProps({
-  variant: {
+  color: {
     type: String,
-    default: 'primary', // primary | dark | disabled
+    default: 'primary-pink',
   },
-  iconLeft: {
-    type: Boolean,
-    default: false,
-  },
-  iconRight: {
-    type: Boolean,
-    default: false,
+  state: {
+    type: String,
+    default: 'default',
   },
   iconOnly: {
     type: Boolean,
     default: false,
   },
-  disabled: {
+  iconPrimary: {
     type: Boolean,
     default: false,
+  },
+  iconSecondary: {
+    type: Boolean,
+    default: false,
+  },
+  textContent: {
+    type: String,
+    default: '',
   },
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '@/styles/variables' as *;
+
 .btn {
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  gap: 16px;
+  color: $color-black;
 
-  padding: 20px 32px;
-  border: none;
+  padding: 16px 32px;
+  border: 1px solid transparent;
   border-radius: 0;
-  font-size: 32px;
-  font-weight: 500;
-  cursor: pointer;
-  transition: background-color 0.2s, color 0.2s;
-}
 
-/* Variantes */
-.btn--primary {
-  background-color: #f6b3bf;
-  color: #000;
-}
-
-.btn--dark {
-  background-color: #000;
-  color: #fff;
-}
-
-.btn--disabled {
-  background-color: #e6e6e6;
-  color: #9a9a9a;
-  cursor: not-allowed;
-}
-
-/* Icônes */
-.btn__icon {
-  font-size: 32px;
+  font-family: $font-family-text;
+  font-size: 75px;
+  font-weight: 400;
   line-height: 1;
-}
 
-/* Bouton icône seule */
-.btn--icon-only {
-  padding: 20px;
-  width: 80px;
-  height: 80px;
-}
+  cursor: pointer;
+  transition: all 0.2s ease;
+  box-sizing: border-box;
 
-.btn:disabled {
-  pointer-events: none;
+  .btn__label {
+    padding: 8px;
+  }
+
+  &--primary-pink {
+    background-color: $color-mba-pink;
+    color: $color-black;
+    border-color: $color-mba-pink;
+
+    &:active:not(:disabled) {
+      opacity: 0.9;
+    }
+
+    &:active:not(:disabled) {
+      transform: scale(0.98);
+    }
+  }
+
+  &--primary-blue {
+    background-color: $color-mba-blue;
+    color: $color-black;
+    border-color: $color-mba-blue;
+
+    &:active:not(:disabled) {
+      opacity: 0.9;
+    }
+
+    &:active:not(:disabled) {
+      transform: scale(0.98);
+    }
+  }
+
+  &--secondary {
+    background-color: $color-black;
+    color: $color-white;
+    border-color: $color-black;
+
+    &:active:not(:disabled) {
+      opacity: 0.9;
+    }
+
+    &:active:not(:disabled) {
+      transform: scale(0.98);
+    }
+  }
+
+  &--tertiary {
+    background-color: $color-white;
+    color: $color-black;
+    border: 1px solid $color-black;
+    border-color: $color-black;
+  
+    &:active:not(:disabled) {
+      opacity: 0.8;
+    }
+
+    &:active:not(:disabled) {
+      transform: scale(0.98);
+    }
+  }
+
+  &:disabled {
+    background-color: $color-grey-light;
+    color: $color-grey-dark;
+    cursor: not-allowed;
+    border-color: transparent;
+    pointer-events: none;
+  }
+
+  &__icon {
+    font-size: 32px;
+    line-height: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: currentColor;
+
+    &--primary {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    :deep(svg) {
+      fill: currentColor;
+      width: 90px;
+      height: 90px;
+    }
+  }
+
+  &--icon-only {
+    padding: 16px;
+
+    .btn__label {
+      display: none;
+    }
+  }
 }
 </style>
