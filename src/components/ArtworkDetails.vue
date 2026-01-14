237b93521bcbe47b0fbbe61d5511dcd5fa604212
Simon Daguet-Kargl <144481209+simdgtk@users.noreply.github.com>
2026-01-14 09:56:43 +0100

    styling

--- src/components/ArtworkDetails.vue
+++ src/components/ArtworkDetails.vue
@@ -1,17 +1,23 @@
 <script setup>
-import {useRoute, useRouter} from "vue-router";
+import { useRoute } from "vue-router";
 import { allData } from '@/store.js';
 import { computed } from 'vue';
-const router = useRouter();
+import ArtworkVueFrame from "./layouts/ArtworkVueFrame.vue";
+
+
+import Button from "./buttons/Button.vue";
+
+import IconFullscreen from '@/components/icons/IconFullscreen.vue';
+import IconPin from "./icons/IconPin.vue";
 const route = useRoute();
 
 const artwork = computed(() => {
   if (allData.value) {
-    const laVille = allData.value.find( ville => ville.slug === route.params.citySlug );
+    const laVille = allData.value.find(ville => ville.slug === route.params.citySlug);
     if (laVille) {
-      const leMusee = laVille.museums.find( m => m.slug === route.params.museumSlug );
+      const leMusee = laVille.museums.find(m => m.slug === route.params.museumSlug);
       if (leMusee) {
-        const leArtwork = leMusee.artworks.find( a => a.slug === route.params.artworkSlug );
+        const leArtwork = leMusee.artworks.find(a => a.slug === route.params.artworkSlug);
         return leArtwork;
       }
     }
@@ -19,75 +25,202 @@ const artwork = computed(() => {
   return null;
 })
 
-const museum = computed(() => {
-  if (allData.value) {
-    const laVille = allData.value.find( ville => ville.slug === route.params.citySlug );
-    if (laVille) {
-      const leMusee = laVille.museums.find( m => m.slug === route.params.museumSlug );
-      return leMusee;
-    }
-  }
-  return null;
-})
-
-const goToNext = () => {
-
-  const isIndex = (element) => element.slug === route.params.artworkSlug;
-
-  if(museum.value.artworks.length > 1) {
-    const indexfinal = museum.value.artworks.findIndex(isIndex)
-    let nextIndex = indexfinal + 1;
-
-    if(museum.value.artworks[nextIndex]){
-      router.push(`/${route.params.citySlug}/${route.params.museumSlug}/${museum.value.artworks[nextIndex].slug}`);
-
-    } else {
-      nextIndex = 0;
-      router.push(`/${route.params.citySlug}/${route.params.museumSlug}/${museum.value.artworks[nextIndex].slug}`);
-    }
-  }
-}
-
-const goToPrev = () => {
-
-  const isIndex = (element) => element.slug === route.params.artworkSlug;
-
-  if(museum.value.artworks.length > 1) {
-    const indexfinal = museum.value.artworks.findIndex(isIndex)
-    let prevIndex = indexfinal - 1;
-
-    if(museum.value.artworks[prevIndex]){
-      router.push(`/${route.params.citySlug}/${route.params.museumSlug}/${museum.value.artworks[prevIndex].slug}`);
-
-    } else {
-      prevIndex = museum.value.artworks.length -1;
-      router.push(`/${route.params.citySlug}/${route.params.museumSlug}/${museum.value.artworks[prevIndex].slug}`);
-    }
-  }
-}
-
 
 </script>
 <template>
-  <div @click.stop v-if="artwork" class="artwork-detail-box">
-    <h2>{{ artwork.name }}</h2>
+  <ArtworkVueFrame>
+    <div v-if="artwork" class="artwork-detail-box">
+      <section class="image-section">
+        <div class="image-container">
+          <img :src="artwork.image" :alt="artwork.name" />
+        </div>
+        <div class="button-container">
+          <Button color="tertiary" text-content="Plein écran" icon-primary>
+            <template #icon-primary>
+              <IconFullscreen />
+            </template>
+          </Button>
+        </div>
 
-    <div v-if="museum.artworks.length > 1">
-      <button @click="goToNext">test</button>
-      <button @click="goToPrev">test</button>
+      </section>
+      <section class="content-section">
+        <div class="artwork-container artwork-infos">
+          <div class="title-and-name">
+            <h2 class="title">{{ artwork.name }}</h2>
+            <p>Jean Dupas</p>
+          </div>
+          <p class="enumeration">{{ artwork.name }}, {{ artwork.year }}, {{ artwork.technique }}, {{ artwork.place }}
+          </p>
+          <div class="artwork-place">
+            <IconPin class="pin" />
+            <span>{{ artwork.place }}</span>
+          </div>
+        </div>
+        <div class="artwork-container artwork-description-container">
+          <div class="scroll-content">
+            <h3 class="documentation">Documentation</h3>
+            <p>{{ artwork.description }}</p>
+          </div>
+        </div>
+      </section>
     </div>
-  </div>
+  </ArtworkVueFrame>
 </template>
 
 <style scoped lang="scss">
 .artwork-detail-box {
-  position: fixed;
-  left: 0;
-  top: 0;
   height: 100%;
   width: 100%;
-  background: white;
+  display: flex;
+
+  @include text-body;
+
+  .image-section {
+    width: 75%;
+    height: 100%;
+    padding: 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+
+    &::before {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0px; // -52 - 5 (border)
+        width: 52px;
+        height: 100%;
+        background-color: $blue-100;
+      }
+    
+      &::after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        right: -5px; // -52 - 5 (border)
+        width: 5px;
+        height: 100%;
+        background-color: $black;
+      }
+
+    .image-container {
+      flex-grow: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      overflow: hidden;
+      background-color: $gray-200;
+      margin: 82px 248px;
+
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+      }
+    }
+
+    .button-container {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      padding-right: 52px; // compenser le bandeau bleu
+    }
+  }
+
+  .content-section {
+    width: 25%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    position: relative;
+
+    .artwork-container {
+      box-sizing: border-box;
+      padding: 32px;
+      min-height: 0;
+    }
+
+    .artwork-infos {
+      flex: 0 0 40%;
+      border-bottom: 5px solid $black;
+      overflow-y: auto;
+
+      &::-webkit-scrollbar {
+        width: 29px; // 24 + 5 for border
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: $black;
+      }
+
+      display: flex;
+      flex-direction: column;
+      gap: 48px;
+
+      .title {
+        font-family: $font-family-headings;
+        line-height: 100%;
+      }
+
+      .artwork-place {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+      
+
+      :deep(svg) {
+        width: 56px;
+        height: 56px;
+      }
+    }
+
+
+  }
+
+  .artwork-description-container {
+    flex: 1;
+    overflow: hidden;
+    min-height: 0;
+    position: relative;
+    padding: 0 !important;
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      bottom: 0;
+      left: 5px;
+      width: calc(100% - 34px); // 5px margins left - 29px scrollbar
+      height: 30%;
+      background: linear-gradient(to top, $white 10%, transparent);
+      z-index: 1;
+      pointer-events: none;
+    }
+
+    .scroll-content {
+      height: 100%;
+      overflow-y: auto;
+      padding: 32px;
+
+      &::-webkit-scrollbar {
+        width: 29px; // 24 + 5 for border
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: $black;
+      }
+    }
+  }
+
+  .documentation {
+    font-weight: 500;
+    font-size: 56px;
+    margin-bottom: 16px;
+  }
 }
-
-
+}
 </style>
