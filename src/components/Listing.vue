4ca918e6b12a9e3c8d32ad02757d8d6087580d46
Hugo Pinchemel <hugo.pinchemel@mmibordeaux.com>
2026-01-15 15:35:17 +0100

    fetch(Listing): update data source to use new Strapi API endpoint

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -97,7 +97,8 @@ const groupedArtworks = computed(() => {
 onMounted(async () => {
   if (!allData.value) {
     try {
-      const response = await fetch("/content/content.json")
+      //const response = await fetch("/content/content.json")
+      const response = await fetch("https://useful-car-6cfb564836.strapiapp.com/api/villes?populate[museums][populate][artworks][populate][images][populate][crops][fields]=*")
       const allPins = await response.json()
       allData.value = allPins.data
     } catch (e) {
